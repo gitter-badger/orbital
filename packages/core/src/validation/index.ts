069ce4d7681fe79b8d9195578d15c8e5970c3306
MonsieurMan <ange.g.picard@gmail.com>
2017-11-28 08:04:30 +0100

    Add Validator function to export from validation

--- packages/core/src/validation/index.ts
+++ packages/core/src/validation/index.ts
@@ -2,9 +2,10 @@ import { integer, range } from './number';
 import { ip, ipv4, ipv6 } from './network';
 import { ValidationContext } from './validation-context';
 
-export { Validator } from './validator';
+export { ValidatorFunction } from './validation-function';
 export { ValidationContext } from './validation-context';
 export { GeneralValidator } from './general-validator';
+export { Validator } from './validator';
 export const Validation = {
   integer,
   ip,
